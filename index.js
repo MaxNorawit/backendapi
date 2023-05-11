3e0de2933461ee1b1d1f1651ce74070b88c744b6
MaxNorawit <maxnorawit@gmail.com>
2023-05-12 00:36:57 +0700

    second edit in index.js

--- index.js
+++ index.js
@@ -169,8 +169,8 @@ app.post("/register", async (req, res) => {
           if (result.length == 0) {
             bcrypt.hash(password, saltRounds, function (err, hash) {
               db.query(
-                "INSERT INTO users (username, password, email) VALUES (?,?,?)",
-                [username, hash, email],
+                "INSERT INTO users (username, password, email, rank) VALUES (?,?,?,?)",
+                [username, hash, email, 0],
                 (err, result) => {
                   if (err) {
                     return res
